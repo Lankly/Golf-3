9c15d59ac398371b8e6eea06ab27ef22a285f5c1
rockon5622 <cmeredit@andrew.cmu.edu>
2018-06-19 13:54:15 -0400

    Added Power

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -6,6 +6,13 @@ public class BallBehavior : MonoBehaviour
 {
     private Rigidbody rb;
     private Camera main;
+    private float speed = 0.0f;
+    //This is the used to time how long the power meter is held
+    private float fireTime = 0.0f;
+    //This is how long it takes to get to max power
+    private float maxPowerTime = 5.0f;
+    //This is what the power is multiplied by
+    private float powerMult = 100.0f;
 
     // Use this for initialization
     void Start()
@@ -18,13 +25,32 @@ public class BallBehavior : MonoBehaviour
     void Update()
     {
         // Clicking anywhere moves ball. No holding to power up shot yet
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && (fireTime < maxPowerTime))
         {
+            //This is where speed is increased
+            fireTime = fireTime + Time.deltaTime;
+        }
+        else if (Input.GetMouseButtonUp(0) && (fireTime < maxPowerTime))
+        {
+            //This is where the ball is fired
+            speed = fireTime * powerMult;
             Vector3 camera_direction = main.transform.forward;
             camera_direction.y = 0;
 
-            rb.AddForce(camera_direction * 1000);
+            rb.AddForce(camera_direction * speed);
+            //INSERT STROKE CODE
         }
+        else if (Input.GetMouseButtonUp(0) && fireTime >= maxPowerTime)
+        {
+            //Overran the power meter
+            fireTime = 0.0f;
+        }
+        else
+        {
+            fireTime = 0.0f;
+        }
+
+       
     }
 
     // Update, but for physics calculations
